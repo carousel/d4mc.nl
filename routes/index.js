d9f3bd0abe664d10aceaa390a2d7d109373026f1
miroslav <miroslav.trninic@gmail.com>
2014-07-28 18:14:31 +0200

    html templates to jade view

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+
+exports.index = function(req, res){
+  res.render('index');
+};
+exports.terms = function(req, res){
+  res.render('terms');
+};
+exports.login = function(req, res){
+  res.render('login');
+};
+exports.ourcontact = function(req, res){
+  res.render('our-contact');
+};
